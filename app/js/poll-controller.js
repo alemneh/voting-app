780f7722bcf7e43749c328e9bb01d6d780d94053
Alemneh <Alemneh@hotmail.com>
2016-06-18 14:11:50 -0700

    testing changes to poll-contoller

--- app/js/poll-controller.js
+++ app/js/poll-controller.js
@@ -28,20 +28,20 @@ module.exports = function(app) {
     let _this = this;
     _this.poll = JSON.parse($window.localStorage.poll);
 
-    _this.updatePoll = function(poll) {
-      console.log(poll);
-      if(poll === 'undefined') {
-        console.log('pick a option');
-      } else {
-        this.poll.options.forEach((ele) => {
-          if(ele.name == poll.option.name) return ele.count++;
-        })
-        $http.put('http://localhost:3000/users/5753835b5aa378cf04a5ab9b/polls/' + _this.poll._id, _this.poll)
-          .then((res) => {
-            $route.reload();
-          }, (err) => console.log(err));
-      }
-    }
+    // _this.updatePoll = function(poll) {
+    //   console.log(poll);
+    //   if(poll === 'undefined') {
+    //     console.log('pick a option');
+    //   } else {
+    //     this.poll.options.forEach((ele) => {
+    //       if(ele.name == poll.option.name) return ele.count++;
+    //     })
+    //     $http.put('http://localhost:3000/users/5753835b5aa378cf04a5ab9b/polls/' + _this.poll._id, _this.poll)
+    //       .then((res) => {
+    //         $route.reload();
+    //       }, (err) => console.log(err));
+    //   }
+    // }
 
   }]);
 };
