3372887e994ee4cad6dbda53f6f71553f7d7aeab
Alemneh <Alemneh@hotmail.com>
2016-09-27 13:13:59 -0700

    added config dir

---
+++ config/enviornment.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  devlopment: {
+    url: 'http://localhost:3000',
+    mode: 'devlopment'
+  },
+  production: {
+    url: 'http://poll-city.herokuapp.com',
+    mode: 'production'
+  }
+};
--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const models = require('./models');
 const userRouter = express.Router();
 const pollRouter = express.Router();
 const loginRouter = express.Router();
-var port = process.env.PORT || 3000;
+const CONFIG = require('./config/enviornment');
+let port = process.env.PORT || 3000;
+let ENV;
+if (process.argv[2]) {
+  ENV = CONFIG.devlopment;
+} else {
+  ENV = CONFIG.production;
+}
 
 
 
@@ -20,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://poll-city.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', ENV.url);
   res.header('Access-Control-Allow-Headers', 'Content-Type, authorization, token');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
@@ -33,4 +40,4 @@ app.use('/users', pollRouter);
 
 
 
-app.listen(port, () => {console.log('port up on '+ port);});
+app.listen(port, () => {console.log('port up on '+ port + ' in ' + ENV.mode + ' mode');});
